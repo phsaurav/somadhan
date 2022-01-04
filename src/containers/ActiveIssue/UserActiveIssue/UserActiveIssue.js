724b811b9262a73091e4eadbbcd4cd1fb1d5ec28
Reshad Sadik <reshadsadik@gmail.com>
2022-01-04 23:41:14 +0600

    issue card details

--- src/containers/ActiveIssue/UserActiveIssue/UserActiveIssue.js
+++ src/containers/ActiveIssue/UserActiveIssue/UserActiveIssue.js
@@ -1,23 +1,31 @@
-import React from "react";
-import Footer from "../../../components/Footer/Footer";
-import IssueCard from "../../../components/IssueCard/IssueCard";
-import Navbar from "../../../components/Navbar/Navbar";
+import React, { useEffect, useState } from 'react';
+import Footer from '../../../components/Footer/Footer';
+import IssueCard from '../../../components/IssueCard/IssueCard';
+import Navbar from '../../../components/Navbar/Navbar';
 
 const UserActiveIssue = () => {
-	return (
-		<div className='flex flex-col md:flex-row'>
-			<div className='md:min-h-screen md:w-80 xl:w-96'>
-				<Navbar></Navbar>
-			</div>
+  const [issues, setIssues] = useState([]);
+  useEffect(() => {
+    fetch('/issueLists.json')
+      .then((res) => res.json())
+      .then((data) => setIssues(data));
+  }, []);
 
-			<div className='flex flex-col justify-between w-full'>
-				<div>
-					<IssueCard></IssueCard>
-				</div>
-				<Footer></Footer>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-col md:flex-row">
+      <div className="md:min-h-screen md:w-80 xl:w-96">
+        <Navbar></Navbar>
+      </div>
+
+      <div className="flex flex-col justify-between w-full">
+        <div>
+          {issues.length &&
+            issues.map((issue) => <IssueCard issue={issue}></IssueCard>)}
+        </div>
+        <Footer></Footer>
+      </div>
+    </div>
+  );
 };
 
 export default UserActiveIssue;
