ebac4aa0dc56097ac694dd8ce16d41fa859e96d3
NasifTazwar <nasiftazwar@gmail.com>
2022-01-05 18:59:17 +0600

    pie chart

--- src/containers/Analytics/Analytics.js
+++ src/containers/Analytics/Analytics.js
@@ -1,79 +1,64 @@
 import React from "react";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
-import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis , ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
 
 const Analytics = () => {
+	const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 	const data = [
-        {
-          name: "Saturday ",
-          camera: 50,
-          sold: 35,
-          amt: 2400
-        },
-        {
-          name: "Sunday",
-          camera: 40,
-          sold: 20,
-          amt: 2210
-        },
-        {
-          name: "Monday",
-          camera: 59,
-          sold: 48,
-          amt: 2290
-        },
-        {
-          name: "Tuesday",
-          camera: 33,
-          sold: 33,
-          amt: 2000
-        },
-        {
-          name: "Wednesday",
-          camera: 88,
-          sold: 50,
-          amt: 2181
-        },
-        {
-          name: "Thursday",
-          camera: 36,
-          sold: 34,
-          amt: 2500
-        },
-        {
-          name: "Friday",
-          camera: 66,
-          sold: 65,
-          amt: 2100
-        }
-      ];
+		{ name: 'Active Issue', value: 400 },
+		{ name: 'Open Issue', value: 300 },
+		{ name: 'Resolved Issues', value: 300 }
+	];
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+	cx, cy, midAngle, innerRadius, outerRadius, percent, index,
+}) => {
+	const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+	const x = cx + radius * Math.cos(-midAngle * RADIAN);
+	const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
 	return (
-		<>
-        <ResponsiveContainer width="95%" height={400}>
-        <LineChart
-            data={data}
-            margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5
-            }}
-        >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Line
-                type="monotone"
-                dataKey="sold"
-                stroke="#8884d8"
-                activeDot={{ r: 8 }} />
-            <Line type="monotone" dataKey="camera" stroke="#82ca9d" />
-        </LineChart>
-            </ ResponsiveContainer></>
+		<text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central" >
+			{`${(percent * 100).toFixed(0)}%`}
+			{data[index].name}
+		</text>
 	);
+};
+  return (
+    <div className="flex flex-col md:flex-row">
+      <div className="md:min-h-screen md:w-80 xl:w-96">
+        <Navbar></Navbar>
+      </div>
+
+      <div className="flex flex-col justify-between w-full">
+        <div className="mt-8">
+            <ResponsiveContainer width="100%" height={800}className="mx-auto container" >
+			<PieChart width={800} height={800}>
+				<Pie
+					data={data}
+					cx={"50%"}
+					cy={"50%"}
+					labelLine={false}
+					label={renderCustomizedLabel}
+					innerRadius="25%" 
+					outerRadius="100%"
+					fill="#8884d8"
+					dataKey="value"
+					paddingAngle={3}
+				>
+					{
+						data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+					}
+				</Pie>
+			</PieChart>
+            </ResponsiveContainer>
+        </div>
+        <Footer></Footer>
+      </div>
+    </div>
+  );
 };
 
 export default Analytics;
