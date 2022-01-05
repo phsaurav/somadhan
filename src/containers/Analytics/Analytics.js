92bf4ee158330f1dbf2b4b206630ff1a266c59a5
NasifTazwar <nasiftazwar@gmail.com>
2022-01-05 17:03:14 +0600

    chart added

--- src/containers/Analytics/Analytics.js
+++ src/containers/Analytics/Analytics.js
@@ -1,21 +1,78 @@
 import React from "react";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
+import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis , ResponsiveContainer } from 'recharts';
 
 const Analytics = () => {
+	const data = [
+        {
+          name: "Saturday ",
+          camera: 50,
+          sold: 35,
+          amt: 2400
+        },
+        {
+          name: "Sunday",
+          camera: 40,
+          sold: 20,
+          amt: 2210
+        },
+        {
+          name: "Monday",
+          camera: 59,
+          sold: 48,
+          amt: 2290
+        },
+        {
+          name: "Tuesday",
+          camera: 33,
+          sold: 33,
+          amt: 2000
+        },
+        {
+          name: "Wednesday",
+          camera: 88,
+          sold: 50,
+          amt: 2181
+        },
+        {
+          name: "Thursday",
+          camera: 36,
+          sold: 34,
+          amt: 2500
+        },
+        {
+          name: "Friday",
+          camera: 66,
+          sold: 65,
+          amt: 2100
+        }
+      ];
 	return (
-		<div className='flex flex-col md:flex-row'>
-			<div className='md:min-h-screen md:w-80 xl:w-96'>
-				<Navbar></Navbar>
-			</div>
-
-			<div className='flex flex-col justify-between w-full'>
-				<div>
-					<h1>This is analytics container</h1>
-				</div>
-				<Footer></Footer>
-			</div>
-		</div>
+		<>
+        <ResponsiveContainer width="95%" height={400}>
+        <LineChart
+            data={data}
+            margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5
+            }}
+        >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line
+                type="monotone"
+                dataKey="sold"
+                stroke="#8884d8"
+                activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="camera" stroke="#82ca9d" />
+        </LineChart>
+            </ ResponsiveContainer></>
 	);
 };
 
